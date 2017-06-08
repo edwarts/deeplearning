d347335d9620dc152d95c4bd52dc5f2674e65768
bo.ma <newtonmark1985@gmail.com>
2017-06-08 04:10:22 +0100

    finish the data capture from cs

--- traderevaluation.py
+++ traderevaluation.py
@@ -1,29 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-
+import utilities as ut
 
 rawCSVData="risktestdata.csv"
 
-rawDf=pd.read_csv(rawCSVData)
+raw_df=ut.get_raw_df(rawCSVData)
 
-# print rawDf
+# build up the training dataset and the model size is 13x13
 
-"Start to normalize the data" \
-"Set the buy as 0 and sell as 1"
-for i,row in rawDf.iterrows():
-    if rawDf.loc[i,'Type']=='buy':
-        rawDf.loc[i,'Type']=0
-    else :
-        rawDf.loc[i,'Type']=1
+matrix14=raw_df.r
 
-"Omit the symbol column"
-del rawDf['Item']
-
-del rawDf['Ticket']
-
-print rawDf
-
-
-# build up the training dataset and the model size is 14x14
-
+print matrix14
---
+++ utilities.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from time import time
+import pandas as pd
+
+def get_time_stamp(str):
+    return long(datetime.strptime(str,"%Y.%m.%d %X").strftime("%s"))
+
+def get_raw_df(raw_file_path):
+    rawDf=pd.read_csv(raw_file_path)
+
+# print rawDf
+
+    "Start to normalize the data" \
+    "Set the buy as 0 and sell as 1"
+    for i,row in rawDf.iterrows():
+        if rawDf.loc[i,'Type']=='buy':
+            rawDf.loc[i,'Type']=0
+        else :
+            rawDf.loc[i,'Type']=1
+        rawDf.loc[i,'StayTime']=(ut.get_time_stamp(rawDf.loc[i,'Close Time'])-ut.get_time_stamp(rawDf.loc[i,'Open Time']))/1000
+
+
+    "Omit the symbol column"
+    del rawDf['Item']
+
+    del rawDf['Ticket']
+
+    del rawDf['Close Time']
+
+    del rawDf['Open Time']
+
+    return rawDf
